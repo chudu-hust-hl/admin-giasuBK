ce182b1ef1c212a6001d7601b815cd310bb2bcb9
Trung Dung <trungdung6789a0@gmail.com>
2025-01-23 11:38:49 +0700

    Change API address

--- src/utils/request.js
+++ src/utils/request.js
@@ -15,7 +15,7 @@ import { useNotification } from "@kyvg/vue3-notification";
 
 const notification = useNotification();
 const service = axios.create({
-  baseURL: "http://202.191.56.71/GiaSuBKAPI/",
+  baseURL: "https://crm.icpc1hn.work/giasuapi/",
   // baseURL: "http://localhost:44375/",
   withCredentials: true, // send cookies when cross-domain requests
   timeout: 20000, // request timeout
